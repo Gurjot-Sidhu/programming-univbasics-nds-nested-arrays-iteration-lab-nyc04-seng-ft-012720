4eb85e35df386224fcd413f02369996a28cf31c4
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-22 00:56:03 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -9,9 +9,9 @@ def join_ingredients(src)
   foods =[]
   counter = 0 
   while counter <src.length do 
-    foods.push("I love #{src[counter][0]} and #{src[0]} on my pizza")
+    foods.push("I love #{src[counter][0]} and #{src[counter][1]} on my pizza")
   end
-  puts "I love #{src} and #{src[0]} on my pizza"
+  
 end
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
